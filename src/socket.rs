295935909bbf640260e9f16ef840d05d61749550
iamazy <iamazy.me@outlook.com>
2023-06-27 21:01:49 +0800

    Hello World!

--- src/socket.rs
+++ src/socket.rs
@@ -1,7 +1,7 @@
 use std::{os::fd::RawFd, sync::Arc};
 
 use nix::{
-    libc::{sa_family_t, sockaddr, sockaddr_ll, AF_PACKET, ETH_P_IP},
+    libc::{sa_family_t, sockaddr, sockaddr_ll, AF_PACKET, ETH_P_ARP, ETH_P_IP},
     sys::socket::{
         bind, recvfrom, sendto, socket, AddressFamily, LinkAddr, MsgFlags, SockFlag, SockProtocol,
         SockType, SockaddrLike, SockaddrStorage,
@@ -9,7 +9,7 @@ use nix::{
     unistd::close,
 };
 
-use crate::{ethernet::EthernetAddress, FireResult};
+use crate::{ethernet::EthernetAddress, EtherType, FireResult, HardwareType};
 
 struct FileDesc {
     fd: RawFd,
@@ -54,7 +54,11 @@ impl Receiver {
     }
 }
 
-pub fn channel(ifindex: usize, mac_addr: EthernetAddress) -> FireResult<(Sender, Receiver)> {
+pub fn channel(
+    ether_type: EtherType,
+    ifindex: usize,
+    mac_addr: EthernetAddress,
+) -> FireResult<(Sender, Receiver)> {
     let socket = socket(
         AddressFamily::Packet,
         SockType::Raw,
@@ -62,13 +66,34 @@ pub fn channel(ifindex: usize, mac_addr: EthernetAddress) -> FireResult<(Sender,
         SockProtocol::EthAll,
     )?;
 
+    let (protocol, hardware_type) = match ether_type {
+        EtherType::Arp => (ETH_P_ARP, HardwareType::Ethernet),
+        EtherType::Ipv4 => (ETH_P_IP, HardwareType::Ethernet),
+    };
+
+    // sockaddr_ll is a device-independent physical-layer (data link layer) address.
+    //
+    // [Further reading](https://man7.org/linux/man-pages/man7/packet.7.html)
     let mut sockaddr = sockaddr_ll {
+        /// Always AF_PACKET
         sll_family: AF_PACKET as sa_family_t,
-        sll_protocol: (ETH_P_IP as u16).to_be(),
-        sll_ifindex: ifindex as i32,
-        sll_hatype: 0,
+        /// Physical-layer protocol
+        sll_protocol: (protocol as u16).to_be(),
+        /// Interface number
+        sll_ifindex: ifindex as libc::c_int,
+        /// ARP hardware type
+        sll_hatype: hardware_type as libc::c_ushort,
+        /// Packet type
+        ///
+        /// PACKET_HOST: 0
+        /// PACKET_BROADCAST: 1
+        /// PACKET_MULTICAST: 2
+        /// PACKET_OTHERHOST: 3
+        /// PACKET_OUTGOING: 4
         sll_pkttype: 0,
+        // Length of MAC address
         sll_halen: 6,
+        // Physical-layer address [MAC]
         sll_addr: [0; 8],
     };
     sockaddr.sll_addr[..6].copy_from_slice(mac_addr.as_bytes());
